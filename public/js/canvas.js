e9a703f7a5de876a166dec3a1bc2a821bf958813
Mohit Ramchandani <Mohitr35@gmail.com>
2013-11-06 18:07:46 -0800

    release 1 motherfuckasss

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,5 +1,23 @@
-var c=document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
-console.log(ctx);
-ctx.fillStyle="#FF0000";
-ctx.fillRect(0,0,150,75);
+var canvasElement=document.getElementById("myCanvas");
+var ctx=canvasElement.getContext("2d");
+canvasElement.width=window.innerWidth;
+canvasElement.height=window.innerHeight;
+ctx.lineWidth="5";
+ctx.strokeStyle="blue";
+var isDrawing = false;
+
+canvasElement.addEventListener("mousedown",function(event){
+	isDrawing = true;
+	ctx.beginPath();
+})
+console.log(isDrawing)
+
+canvasElement.addEventListener("mousemove",function(event){
+	if(!isDrawing){ return }
+	ctx.lineTo(event.clientX,event.clientY);
+	ctx.stroke();
+})
+
+canvasElement.addEventListener("mouseup",function(){
+	isDrawing = false;	
+})
